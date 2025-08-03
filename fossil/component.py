3c32903e866a979c8e6c4d0cda36648ba707baa4
claudio vestini <cvestini@gmail.com>
2025-08-03 15:45:33 +0100

    Fix component

--- fossil/component.py
+++ fossil/component.py
@@ -10,7 +10,7 @@ class Component:
         pass
 
     def get(self, **kw):
-        raise NotImplemented("Not implemented in " + self.__class__.__name__)
+        raise NotImplementedError("Not implemented in " + self.__class__.__name__)
 
     def to_next_component(self, out, component, **kw):
         return out
